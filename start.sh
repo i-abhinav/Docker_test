1b7e1217fd51240d6cc8d633712c54a736ce47fc
Abhinav Gupta <abhinav.gupta02@nagarro.com>
2019-09-03 17:21:21 +0530

    docker file changed

--- start.sh
+++ start.sh
@@ -9,7 +9,8 @@ white=$'\e[0m'
 source .env
 
 echo " $red <<<<<< Setting up Docker Environment >>>>>> $white "
-docker-compose down && docker-compose up --build -d
+# docker-compose down && docker-compose up --build -d
+docker-compose down && docker-compose up --build -d && docker-compose logs -f
 
 echo " $grn <<<<<< Installing Dependencies >>>>>> $blu "
 #Sleep for 150seconds
@@ -30,29 +31,40 @@ vendor_present() {
   fi
 
 # docker exec ${APP_NAME}_php bash -c 'chmod 777 -R /var/www/html'
-docker exec ${APP_NAME}_php bash -c 'chmod 777 -R storage'
+# docker exec ${APP_NAME}_php bash -c 'chmod 777 -R storage'
+docker exec -it ${APP_NAME}_app bash -c "sudo -u devuser /bin/bash"
 
-docker exec ${APP_NAME}_php php artisan config:cache
+# docker exec ${APP_NAME}_php php artisan config:cache
+php artisan config:cache
 
-docker exec ${APP_NAME}_php php artisan optimize:clear
+# docker exec ${APP_NAME}_php php artisan optimize:clear
+php artisan optimize:clear
 
 echo " $red <<<<<< Running Migrations & Data Seeding >>>>>> $white "
-docker exec ${APP_NAME}_php php artisan key:generate
-docker exec ${APP_NAME}_php php artisan migrate
-docker exec ${APP_NAME}_php php artisan db:seed
+# docker exec ${APP_NAME}_php php artisan key:generate
+# docker exec ${APP_NAME}_php php artisan migrate
+# docker exec ${APP_NAME}_php php artisan db:seed
+
+php artisan key:generate
+php artisan migrate
+php artisan db:seed
 # docker exec ${APP_NAME}_php php artisan db:seed
 
 echo " $red <<<<<< Running PHP in-built server >>>>>> $white "
-docker exec ${APP_NAME}_php php -S localhot:8080 -t public
+# docker exec ${APP_NAME}_php php -S localhot:8080 -t public
+php -S localhot:8080 -t public
 
 echo " $red <<<<<< Running MyOrder All Test Cases >>>>>> $white "
-docker exec ${APP_NAME}_php ./vendor/bin/phpunit
+# docker exec ${APP_NAME}_php ./vendor/bin/phpunit
+./vendor/bin/phpunit
 
 echo " $red <<<<<< Running MyOrder Integration Test Cases >>>>>> $white "
-docker exec ${APP_NAME}_php ./vendor/bin/phpunit OrderIntegrationTest
+# docker exec ${APP_NAME}_php ./vendor/bin/phpunit OrderIntegrationTest
+./vendor/bin/phpunit OrderIntegrationTest
 
 echo " $red <<<<<< Running MyOrder Unit Test Cases >>>>>> $white "
-docker exec ${APP_NAME}_php ./vendor/bin/phpunit OrderControllerTest
+# docker exec ${APP_NAME}_php ./vendor/bin/phpunit OrderControllerTest
+./vendor/bin/phpunit OrderControllerTest
 
 echo " $red <<<<<< Wanna check Swagger Implemetatipn >>>>>> $white "
 echo "http://localhost:8080/swagger/" 
